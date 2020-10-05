184696299ab3bb0f69fd3a01f50260fcc22741e6
kate <wondalisa@gmail.com>
2020-10-05 19:55:55 +0300

    Операторы Continue & Break

--- key_words.py
+++ key_words.py
@@ -8,5 +8,18 @@ def collection_words():
     else:
         print("два первых условия не выполнены")
 
+    copy_of_key = key_words_for_collection[2:3]
+    print(copy_of_key)
+
+    for copy_of_key in range(3):
+        if copy_of_key == 1:
+            continue
+        if copy_of_key == 3:
+            break
+        print(copy_of_key)
+
+    for key_words_for_collection in range(6):
+        print("довольно")
+
 
 collection_words()
