089634009f9855c0d02f4b3248caa2554d184ac6
kate <wondalisa@gmail.com>
2020-10-06 10:58:52 +0300

    Оператор while

--- key_words.py
+++ key_words.py
@@ -1,5 +1,4 @@
-def collection_words():
-    key_words_for_collection = ["boy", "summer", 234, "\t"]
+def collection_words(key_words_for_collection: list):
     len(key_words_for_collection)
     if "boys" in key_words_for_collection:
         print("Ok")
@@ -8,18 +7,30 @@ def collection_words():
     else:
         print("два первых условия не выполнены")
 
-    copy_of_key = key_words_for_collection[2:3]
-    print(copy_of_key)
+    copy_of_key_list = key_words_for_collection[2:4]
+    if 234 in copy_of_key_list:
+        print(copy_of_key_list)
 
-    for copy_of_key in range(3):
-        if copy_of_key == 1:
-            continue
-        if copy_of_key == 3:
-            break
-        print(copy_of_key)
+    index = 0
+    while (index < len(copy_of_key_list)):
+        element = copy_of_key_list[index]
 
-    for key_words_for_collection in range(6):
-        print("довольно")
+        if (str(element).isdigit() and element > 100):
+            print(element, "больше 10")
+
+        print(f"Index: {index}")
+        index += 1
 
 
-collection_words()
+    # for copy_of_key in range(3):
+    #     if copy_of_key == 1:
+    #         continue
+    #     if copy_of_key == 3:
+    #         break
+    #     print(copy_of_key)
+    #
+    # for key_words_for_collection in range(6):
+    #     print("довольно")
+
+
+collection_words(["boy", "summer", 234, "\t"])
