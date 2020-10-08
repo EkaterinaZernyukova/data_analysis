036f4b918d22ecfc6758ed5ee4c629cf78173608
kate <wondalisa@gmail.com>
2020-10-08 20:46:20 +0300

    apple.test+plot

--- helpers/file_helper.py
+++ helpers/file_helper.py
@@ -2,5 +2,4 @@ import os
 
 
 def get_resource_file_path(resource_name: str):
-    return (os.path.join(os.path.abspath(os.curdir), 'resources',  resource_name))
-
+    return (os.path.join(os.path.abspath(os.curdir), 'resources', resource_name))
--- pandas_enter_file.py
+++ pandas_enter_file.py
@@ -1,4 +1,5 @@
 import pandas as pn
+import matplotlib.pyplot as ml
 
 # import titanic.csv
 from helpers import file_helper
@@ -10,4 +11,13 @@ print(titanic_test.groupby(["PClass", "Survived"])["PassengerID"].count())
 pvt = titanic_test.pivot_table(index=["Sex"], columns=["PClass"], values="Name", aggfunc="count")
 print(pvt.loc["female", ["1st", "2nd", "3rd"]])
 
+apple_test = pn.read_csv(file_helper.get_resource_file_path("apple.csv"), index_col="Date", parse_dates=True)
+apple_test = apple_test.sort_index()
+print(apple_test.info())
+print(apple_test.loc["2012-Feb", "Close"].mean())
+print(apple_test.loc["2012-Feb":"2015-Feb", "Close"].mean())
+print(apple_test.resample("W")["Close"].mean())
 
+for_view = apple_test.loc["2012-Feb":"2017-Feb", "Close"]
+for_view.plot()
+ml.show()
