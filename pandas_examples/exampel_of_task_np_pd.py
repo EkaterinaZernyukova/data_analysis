7316d57e91850ed077098fd2363bc49662fc6070
kate <wondalisa@gmail.com>
2020-10-14 16:50:54 +0300

    Task from professor and problem with quantity

--- pandas_examples/exampel_of_task_np_pd.py
+++ pandas_examples/exampel_of_task_np_pd.py
@@ -50,27 +50,63 @@ print(cos_grid - sin_grid)
 # в) создайте логическую функцию close(x, y, eps), которая возвращает True, если  |x−y|<ε , и False в противном случае;
 # г) примените данную функцию к двум массивам, созданным выше;
 print("Логическая функция")
-# x = cos_grid
-# y = sin_grid
-# eps = 1e-6
-# new = np.abs(x - y)<0.0005
-# print(new)
 
 
 def close(x, y, eps):
-    x=cos_grid
-    y=sin_grid
-    eps=1e-6
-    new=np.abs(x - y)
-    if new < eps:
-        print(True)
-    else:
-        return False
+    return np.abs(x - y) < eps
 
-    # д) убедитесь, что все значения в полученном массиве равны True.
-    result = np.abs(x - y) < 0.0005
-    print(result)
 
+print(close(cos_grid, sin_grid, 1e-6))
+
+# # д) убедитесь, что все значения в полученном массиве равны True.
+# Во всех языках программирования 1е-6 это = 0,000 001. Это 1 умножить на 10 в минус шестой степени - сокращенная запись.
+# E (exponent) — буква E, означающая «*10^»
+
+
+close_vector = np.vectorize(close)
+print(close_vector(cos_grid, sin_grid, 0.0000000000000000000000000000000000000000005))
+
+# Создайте матрицу (двумерный массив) из нормального распределения с параметрами  μ=5 ,  σ=2.5  размером  10×5
+#  Выборка из нормального распределения со средним 5, стандартным отклонением 2.5
+matrica = np.random.normal(5, 2.5, (10, 5))
+print(matrica)
+
+# Найдите среднее значение в 5-й строке.
+print("5я строка")
+print(matrica[4])
+print(matrica[4, :])
+print(np.mean(matrica[4:]))
+
+# Найдите медиану 3-го столбца.
+print("3й столбец")
+print(matrica[:, 2])
+print(np.median(matrica[:, 2]))
+
+# Посчитайте сумму элементов матрицы: а) по строкам; б) по столбцам; в) сумму всех элементов.
+print("Суммы")
+print(np.sum(matrica, axis=1))
+print(np.sum(matrica, axis=0))
+print(np.sum(matrica))
+
+# Вычислите определитель (детерминант) подматрицы размером  3×3 , расположенной в левом верхнем углу исходной матрицы.
+# Вычисление определителя нужно чтобы узнать, обратима ли матрица.
+# Узнавать, обратима ли матрица, нужно для того, чтобы решить: обращать её таки - а это сложная алгоритмическая задача - или нет.
+# Обращать матрицы нужно для вычисления коэффициентов линейной регрессии.
+# А линейная регрессия - один из основных инструментов в статистике, которая нужна для предсказания результатов.
+print(matrica[:3, :3])
+print(np.linalg.det(matrica[:3, :3]))
+
+print("Обратная матрица")
+print(np.linalg.inv(matrica[:3, :3]))
+
+# Найдите количество строк с суммой элементов меньше средней суммы по строкам.
+print([np.sum(matrica, axis=1) < np.mean(matrica, axis=1)])
+print("Средняя сумма по строкам")
+print(np.mean(matrica, axis=1))
+print("Сумма элементов по строкам")
+print(np.sum(matrica, axis=1))
+
+print(np.shape(matrica[0]))
 # cardio_train_test = pd.read_csv(file_helper.get_resource_file_path("cardio_train.csv"))
 # print(cardio_train_test.head())
 #
