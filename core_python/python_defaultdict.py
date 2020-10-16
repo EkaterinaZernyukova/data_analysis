cd8638f3c304d9a6e9eec72974035790b8dd0ccc
kate <wondalisa@gmail.com>
2020-10-16 12:22:38 +0300

    jupyter notebook margin USA

--- core_python/python_defaultdict.py
+++ core_python/python_defaultdict.py
@@ -1,18 +1,26 @@
-from collections import Counter
+# from collections import Counter
+#
+#
+# def most_common_count_words():
+#     word_counts = Counter({1: 2, 3: 1, 2: 3, 6: 8, 4: 12})
+#     print(word_counts)
+#
+#     for word, count in word_counts.most_common(3):
+#         print(word, count)
+#
+#     word_counts[99] = 18
+#     word_counts[106] = 344
+#     word_counts.update({123: 124, 2345: 1234})
+#
+#     print(word_counts.most_common(3))
+#
+#
+# most_common_count_words()
 
 
-def most_common_count_words():
-    word_counts = Counter({1: 2, 3: 1, 2: 3, 6: 8, 4: 12})
-    print(word_counts)
-
-    for word, count in word_counts.most_common(3):
-        print(word, count)
-
-    word_counts[99] = 18
-    word_counts[106] = 344
-    word_counts.update({123: 124, 2345: 1234})
-
-    print(word_counts.most_common(3))
-
-
-most_common_count_words()
+import numpy as np
+def square(x):
+    return x * x
+x = np.random.randint(1, 10)
+y = square(x)
+print('%d squared is %d' % (x, y))
