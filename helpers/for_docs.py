090acdb754dc0d11046f55520ec787127affd5fd
kate <wondalisa@gmail.com>
2020-10-25 22:11:18 +0300

    mpl варииации с оформением графиков

--- helpers/for_docs.py
+++ helpers/for_docs.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib as mpl
+print(mpl.__version__)
 
-url_template = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=5415&Year={year}&Month={month}&timeframe=1&submit=Download+Data"
+x = np.linspace(0, 10, 100)
 
+plt.plot(x, np.sin(x))
+plt.plot(x, np.cos(x))
 
-def download_wether(year, month):
-    url = url_template.format(year=year, month=month)
-    weather_date = pd.read_csv(url, index_col="Date/Time", parse_dates=True, encoding="latin1")
-    weather_date = weather_date.dropna(axis=1, how="any")
-    weather_date.columns = [col.replace('\xb0', '') for col in weather_date.columns]
-    weather_date = weather_date.drop(["Latitude (y)", "Station Name", "Climate ID", "Year", "Month"], axis=1)
-    return weather_date
-
-request["Incident Zip"]=request["Incident Zip"].str.slice(0,5)
+plt.show()
